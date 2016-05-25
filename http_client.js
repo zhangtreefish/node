bc7d33b0532fd6169912f497c4c09f4c7b965fb0
zhangtreefish <zhangtreefish@users.noreply.github.com>
2016-05-25 02:23:37 +0000

    refactor: leaner code

--- http_client.js
+++ http_client.js
@@ -1,13 +1,8 @@
-var http=require("http");
-http.get(process.argv[2], function(res){
-    res.setEncoding('utf8')
-    .on("data", function(data){
-        console.log(data);
-    })
-    .on('error', function(err){
-        console.error(err); 
-    })
+var http = require('http');  
+       
+http.get(process.argv[2], function (response) {  
+    response.setEncoding('utf8')  
+    .on('data', console.log)  
+    .on('error', console.error)  
 })
-.on('error', function(err){
-    console.error(err); 
-});  
+.on('error', console.error);  
