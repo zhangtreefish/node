e74c89f904412a6db9fb57705406daf9997a5cfa
zhangtreefish <zhangtreefish@users.noreply.github.com>
2016-08-01 03:06:31 +0000

    feat: implement http get

--- http_client.js
+++ http_client.js
@@ -1,8 +1,13 @@
+
 var http = require('http');  
        
-http.get(process.argv[2], function (response) {  
+http.get(process.argv[2], function cb(response) {  
     response.setEncoding('utf8')  
-    .on('data', console.log)  
-    .on('error', console.error)  
+    // .on('data', function(data){
+    //     console.log(data);})  
+    // .on('error', function(error){
+    //     console.error(error);}); 
+    .on('data', console.log)
+    .on('error', console.error);
 })
 .on('error', console.error);  
